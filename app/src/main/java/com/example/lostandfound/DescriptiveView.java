cd964b9f2adf41a6d5172f6dcec0879c6cf35556
NAFISA ALAM <37190335+NafuKrypto@users.noreply.github.com>
2021-06-24 04:34:07 +0600

    Image Slider added , retreived from github

--- app/src/main/java/com/example/lostandfound/DescriptiveView.java
+++ app/src/main/java/com/example/lostandfound/DescriptiveView.java
@@ -1,14 +1,177 @@
 package com.example.lostandfound;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 public class DescriptiveView extends AppCompatActivity {
+    Toolbar toolbar;
+    TextView textView;
+    private List<FeedItemDataRetrieve> listDataFeed;
+    private FeedDataAdapter adapterFeed;
+    String what,desc,time,timestampString,primaryKey,category1,category2,address;
+    String cat2,timeStamp,type;
+    ViewPager viewPager;
+    ViewPagerAdapter adapter;
+    List<ImageClass> listImage;
+    private String [] imageUrls =new String[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_descriptive_view );
+        toolbar = (Toolbar) findViewById( R.id.toolbar );
+
+        setSupportActionBar( toolbar );
+        getSupportActionBar().setHomeAsUpIndicator( R.drawable.back_button );
+        getSupportActionBar().setDisplayHomeAsUpEnabled( true );
+        getSupportActionBar().setDisplayShowTitleEnabled(false); // hide the current title from the Toolbar
+
+        textView=findViewById( R.id.text );
+        Intent intentFromAdapter=getIntent();
+        primaryKey= intentFromAdapter.getStringExtra("primaryKey");
+        what=intentFromAdapter.getStringExtra("what");
+        time=intentFromAdapter.getStringExtra("time");
+        desc=intentFromAdapter.getStringExtra("desc");
+        timestampString=intentFromAdapter.getStringExtra("timestamp");
+        category1=intentFromAdapter.getStringExtra("category1");
+        category2=intentFromAdapter.getStringExtra("category2");
+        address=intentFromAdapter.getStringExtra("address");
+        //int * positionValue= &primaryKey;
+
+        //ViewPager viewPager = findViewById(R.id.view_pager);
+
+        viewPager=findViewById( R.id.view_pager );
+        //ViewPagerAdapter adapter = new ViewPagerAdapter(this, imageUrls);
+        //viewPager.setAdapter(adapter);
+        retrieveOtherData( primaryKey );
+        retrieveImageUrl( primaryKey );
+        textView.setText(what+" "+ primaryKey+" "+address+"  "+category1+" "+category2+" "
+        +desc+" "+timestampString+" "+time+" ");
+
+        /*
+        "address",ld.getAddress() );
+                intent.putExtra( "desc",ld.getDesc() );
+                intent.putExtra( "time",ld.getTime() );
+                intent.putExtra( "timestamp",ld.getTimestamp() );
+                intent.putExtra( "category1",ld.getCategory1() );
+                intent.putExtra( "category2",ld.getCategory2() );
+                */
+
+
+    }
+
+    private void retrieveImageUrl(String primaryKey) {
+        listImage=new ArrayList<>(  );
+        DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference("postImage").child( primaryKey );
+        databaseReference.addListenerForSingleValueEvent( new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                int i=0;
+                if (snapshot.exists()){
+                    for (DataSnapshot dataSnapshot:snapshot.getChildren()){
+                         // imageUrls=imageUrls+", "+dataSnapshot.getValue();
+                        //imageUrls=dataSnapshot.getValue().toString();
+                       ImageClass imageclassUri=dataSnapshot.getValue(ImageClass.class);
+
+                        //listImage.add( imageclassUri);
+                        try{ imageUrls[i]=imageclassUri.getImageLink();
+                            Toast.makeText( getApplicationContext(),imageclassUri.getImageLink(),Toast.LENGTH_LONG ).show();
+                            Toast.makeText( getApplicationContext(),imageUrls[i],Toast.LENGTH_LONG ).show();
+                         i++;}catch (Exception e){
+                            Toast.makeText( getApplicationContext(), e.getMessage(),Toast.LENGTH_LONG ).show();
+                        }
+                    }
+                     adapter=new ViewPagerAdapter( getApplicationContext(),imageUrls);
+                      if(adapter==null){
+                          Toast.makeText( getApplicationContext(),"null",Toast.LENGTH_SHORT ).show();
+                      }
+                      try {
+                          viewPager.setAdapter( adapter );
+                      } catch (Exception e){
+                          Toast.makeText( getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT ).show();
+                      }
+                     //textView.setText( imageUrls[0]+" "+imageUrls[1]+" "+imageUrls[2] );
+                    textView.setText( String.valueOf( listImage ) );
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        } );
+
+
+    }
+
+    protected void retrieveOtherData(String primaryKey){
+
+        DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference("feed");
+        databaseReference.orderByChild( primaryKey ).addListenerForSingleValueEvent( new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                   for (DataSnapshot dataSnapshot:snapshot.getChildren()){
+                        cat2=dataSnapshot.child( "category2" ).getValue().toString();
+                        timeStamp=dataSnapshot.child( "timestamp" ).getValue().toString();
+                        type=dataSnapshot.child( "type" ).getValue().toString();
+                   }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        } );
+
+
+
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent=new Intent( getApplicationContext(),MainActivity.class );
+                startActivity( intent );
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public  static class ImageClass{
+        public String imageLink;
+        public ImageClass() {
+        }
+        public ImageClass(String imageLink){
+            this.imageLink=imageLink;
+        }
+
+        public String getImageLink() {
+            return imageLink;
+        }
+
+        public void setImageLink(String imageLink) {
+            this.imageLink = imageLink;
+        }
     }
 }
