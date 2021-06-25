2885fb504305ff07d19294ce13cf0a8875a55b8b
NAFISA ALAM <37190335+NafuKrypto@users.noreply.github.com>
2021-06-25 09:52:39 +0600

    qrcode database pass

--- app/src/main/java/com/example/lostandfound/QRCodeForm.java
+++ app/src/main/java/com/example/lostandfound/QRCodeForm.java
@@ -25,6 +25,7 @@ Resources resources;
 Button submitButton;
 int selectItem=0;
 TextView textView;
+String s="None";
 String nameS,phonenumS,addressS,emailS,area,selectCat,modelS,email2S,address2S,ownername2S,phonenum2S,name3S;
 String email3S,office3S,phone3S,address3S,networkName,NetworkType,networkPass,Url;
 //RadioButton camera,phone,laptop,studentId,officeId,atmCard,nid,moneyBag,passport,bag,chequeBook,houseDoor;
@@ -86,64 +87,125 @@ int selectedId;
               switch (selectItem) {
                   case 1:
                       createAlertDialougeFuncRealPhone();
-                      
+
+                      qrCodeDatabaseClass database=new qrCodeDatabaseClass( s,s,s,s,area,selectCat,modelS,email2S,address2S,ownername2S,phonenum2S,
+                              s,s,s,s,s,s,s,s,s);
+                      database.writeToDatabase();
                       break;
                   case 2:
                       createAlertDialougeFuncRealPhone();
+                      qrCodeDatabaseClass database1=new qrCodeDatabaseClass( s,s,s,s,area,selectCat,modelS,email2S,address2S,ownername2S,phonenum2S,
+                              s,s,s,s,s,s,s,s,s);
+                      database1.writeToDatabase();
                       break;
                   case 3:
                       createAlertDialougeFuncRealPhone();
+                      qrCodeDatabaseClass database3=new qrCodeDatabaseClass( s,s,s,s,area,selectCat,modelS,email2S,address2S,ownername2S,phonenum2S,
+                              s,s,s,s,s,s,s,s,s);
+                      database3.writeToDatabase();
                       break;
                   case 4:
                       createAlertDialougeFuncPhone();
+                      qrCodeDatabaseClass database4=new qrCodeDatabaseClass( nameS,phonenumS,addressS,emailS,
+                              area,selectCat,s,s,s,s,s,s,s,s,s,s,s,s,s,s);
+                      database4.writeToDatabase();
                       break;
                   case 5:
                       createAlertDialougeFuncPhone();
+                      qrCodeDatabaseClass database5=new qrCodeDatabaseClass( nameS,phonenumS,addressS,emailS,
+                              area,selectCat,s,s,s,s,s,s,s,s,s,s,s,s,s,s);
+                      database5.writeToDatabase();
                       break;
                   case 6:
                       createAlertDialougeFuncStudId();
+                      qrCodeDatabaseClass database6=new qrCodeDatabaseClass(s,s,s,s,area,selectCat,
+                              s,s,s,s,s,name3S,email3S,office3S,phone3S,address3S,s,s,s,s);
+                      database6.writeToDatabase();
+
                       break;
                   case 7:
                       createAlertDialougeFuncStudId();
+                      qrCodeDatabaseClass database7=new qrCodeDatabaseClass(s,s,s,s,area,selectCat,
+                              s,s,s,s,s,name3S,email3S,office3S,phone3S,address3S,s,s,s,s);
+                      database7.writeToDatabase();
                       break;
                   case 8:
                       createAlertDialougeFuncPhone();
+                      qrCodeDatabaseClass database8=new qrCodeDatabaseClass( nameS,phonenumS,addressS,emailS,
+                              area,selectCat,s,s,s,s,s,s,s,s,s,s,s,s,s,s);
+                      database8.writeToDatabase();
                       break;
                   case 9:
                       createAlertDialougeFuncPhone();
+                      qrCodeDatabaseClass database9=new qrCodeDatabaseClass( nameS,phonenumS,addressS,emailS,
+                              area,selectCat,s,s,s,s,s,s,s,s,s,s,s,s,s,s);
+                      database9.writeToDatabase();
                       break;
                   case 10:
                       createAlertDialougeFuncPhone();
+                      qrCodeDatabaseClass database10=new qrCodeDatabaseClass( nameS,phonenumS,addressS,emailS,
+                              area,selectCat,s,s,s,s,s,s,s,s,s,s,s,s,s,s);
+                      database10.writeToDatabase();
                       break;
                   case 11:
                       createAlertDialougeFuncPhone();
+                      qrCodeDatabaseClass database11=new qrCodeDatabaseClass( nameS,phonenumS,addressS,emailS,
+                              area,selectCat,s,s,s,s,s,s,s,s,s,s,s,s,s,s);
+                      database11.writeToDatabase();
                       break;
                   case 12:
                       createAlertDialougeFuncPhone();
+                      qrCodeDatabaseClass database12=new qrCodeDatabaseClass( nameS,phonenumS,addressS,emailS,
+                              area,selectCat,s,s,s,s,s,s,s,s,s,s,s,s,s,s);
+                      database12.writeToDatabase();
                       break;
                   case 13:
                       createAlertDialougeFuncPhone();
+                      qrCodeDatabaseClass database13=new qrCodeDatabaseClass( nameS,phonenumS,addressS,emailS,
+                              area,selectCat,s,s,s,s,s,s,s,s,s,s,s,s,s,s);
+                      database13.writeToDatabase();
                       break;
                   case 14:
                       createAlertDialougeFuncPhone();
+                      qrCodeDatabaseClass database14=new qrCodeDatabaseClass( nameS,phonenumS,addressS,emailS,
+                              area,selectCat,s,s,s,s,s,s,s,s,s,s,s,s,s,s);
+                      database14.writeToDatabase();
                       break;
                   case 15:
                       createAlertDialougeFuncPhone();
+                      qrCodeDatabaseClass database15=new qrCodeDatabaseClass( nameS,phonenumS,addressS,emailS,
+                              area,selectCat,s,s,s,s,s,s,s,s,s,s,s,s,s,s);
+                      database15.writeToDatabase();
                       break;
                   case 16:
                       createAlertDialougeFuncPhone();
+                      qrCodeDatabaseClass database16=new qrCodeDatabaseClass( nameS,phonenumS,addressS,emailS,
+                              area,selectCat,s,s,s,s,s,s,s,s,s,s,s,s,s,s);
+                      database16.writeToDatabase();
                       break;
                   case 17:
                       createAlertDialougeFuncUrl();
+                      qrCodeDatabaseClass database17=new qrCodeDatabaseClass(s,s,s,s,area,selectCat,
+                              s,s,s,s,s,s,s,s,s,s,s,s,s, Url );
+                      database17.writeToDatabase();
                       break;
                   case 18:
                       createAlertDialougeFuncUrl();
+                      qrCodeDatabaseClass database18=new qrCodeDatabaseClass(s,s,s,s,area,selectCat,
+                              s,s,s,s,s,s,s,s,s,s,s,s,s, Url );
+                      database18.writeToDatabase();
                       break;
                   case 19:
                       createAlertDialougeFuncWifi();
+                      qrCodeDatabaseClass database19=new qrCodeDatabaseClass(s,s,s,s,area,selectCat,
+                              s,s,s,s,s,s,s,s,s,s,networkName,NetworkType,networkPass, s );
+                      database19.writeToDatabase();
                       break;
                   case 20:
                       createAlertDialougeFuncPhone();
+                      qrCodeDatabaseClass database20=new qrCodeDatabaseClass( nameS,phonenumS,addressS,emailS,
+                              area,selectCat,s,s,s,s,s,s,s,s,s,s,s,s,s,s);
+                      database20.writeToDatabase();
                       break;
 
               }
@@ -428,6 +490,7 @@ int selectedId;
             case R.id.phone:
                 if (checked){
                     selectItem=1;
+                    selectCat="phone";
                     //createAlertDialougeFuncPhone();
                     Toast.makeText( getApplicationContext(),"phone is checked",Toast.LENGTH_LONG ).show();
 
@@ -438,42 +501,49 @@ int selectedId;
             case R.id.camera:
                 if (checked){
                     selectItem=2;
+                    selectCat="camera";
                 }
                     // Ninjas rule
                     break;
             case R.id.laptop:
                 if (checked){
                     selectItem=3;
+                    selectCat="laptop";
                 }
                     // Ninjas rule
                     break;
             case R.id.atm_card:
                 if (checked){
                     selectItem=4;
+                    selectCat="atmcard";
                 }
                     // Ninjas rule
                     break;
             case R.id.key_ring:
                 if (checked){
                     selectItem=5;
+                    selectCat="keyring";
                 }
                     // Ninjas rule
                     break;
             case R.id.student_id:
                 if (checked){
                     selectItem=6;
+                    selectCat="studentid";
                 }
                     // Ninjas rule
                     break;
             case R.id.officeId:
                 if (checked){
                     selectItem=7;
+                    selectCat="officeid";
                 }
                     // Ninjas rule
                     break;
             case R.id.nid:
                 if (checked){
                     selectItem=8;
+                    selectCat="nid";
                 }
                     // Ninjas rule
                     break;
@@ -481,72 +551,84 @@ int selectedId;
             case R.id.money_bag:
             if (checked){
                 selectItem=9;
+                selectCat="moneybag";
             }
                 // Ninjas rule
                 break;
             case R.id.bag:
                 if (checked){
                     selectItem=10;
+                    selectCat="bag";
                 }
                     // Ninjas rule
                     break;
             case R.id.passport:
                 if (checked){
                     selectItem=11;
+                    selectCat="passport";
                 }
                     // Ninjas rule
                     break;
             case R.id.cheque_book:
                 if (checked){
                     selectItem=12;
+                    selectCat="chequebook";
                 }
                     // Ninjas rule
                     break;
             case R.id.house_door:
                 if (checked){
                     selectItem=13;
+                    selectCat="housedoor";
                 }
                     // Ninjas rule
                     break;
             case R.id.book:
                 if (checked){
                     selectItem=14;
+                    selectCat="book";
                 }
                     // Ninjas rule
                     break;
             case R.id.dairy:
                 if (checked){
                     selectItem=15;
+                    selectCat="dairy";
                 }
                     // Ninjas rule
                     break;
             case R.id.document_file:
                 if (checked){
                     selectItem=16;
+                    selectCat="documentfile";
                 }
                     // Ninjas rule
                     break;
             case R.id.youtube:
                 if (checked){
                     selectItem=17;
+                    selectCat="youtube";
                 }
                     // Ninjas rule
                     break;
             case R.id.facebook:
                 if (checked){
                     selectItem=18;
+                    selectCat="facebook";
                 }
                     // Ninjas rule
                     break;
             case R.id.wifi:
                 if (checked){
                     selectItem=19;
+                    selectCat="wifi";
                 }
                     // Ninjas rule
                     break;
             case R.id.othersId:
                 if (checked){
                     selectItem=20;
+                    selectCat="othersid";
                 }
                     // Ninjas rule
                     break;
